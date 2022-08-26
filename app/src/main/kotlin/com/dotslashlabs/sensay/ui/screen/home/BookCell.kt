6eefc6a19fbf4ff53d3c8cb494616854aba0c117
Shirish Kamath <shirishk.87@gmail.com>
2022-08-26 15:19:34 +0530

    Better data spacing in grid

--- app/src/main/kotlin/com/dotslashlabs/sensay/ui/screen/home/BookCell.kt
+++ app/src/main/kotlin/com/dotslashlabs/sensay/ui/screen/home/BookCell.kt
@@ -1,5 +1,6 @@
 package com.dotslashlabs.sensay.ui.screen.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,13 +9,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import com.dotslashlabs.sensay.ui.screen.common.CoverImage
 import com.dotslashlabs.sensay.ui.screen.home.library.OnNavToBook
+import data.entity.Book
+import data.entity.BookProgress
 import data.entity.BookProgressWithBookAndChapters
+import data.entity.Chapter
+import data.util.ContentDuration
+import kotlin.time.Duration.Companion.hours
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,72 +47,110 @@ fun BookCell(
 
 @Composable
 private fun GridBookView(
-    @Suppress("UNUSED_PARAMETER") bookProgressWithChapters: BookProgressWithBookAndChapters,
-    @Suppress("UNUSED_PARAMETER") height: Dp = 270.dp,
+    bookProgressWithChapters: BookProgressWithBookAndChapters,
+    height: Dp = 240.dp,
+    authorBackgroundColor: Color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8F),
 ) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxWidth()
             .height(height),
+        verticalArrangement = Arrangement.Top,
     ) {
-
-        CoverImage(
-            coverUri = bookProgressWithChapters.book.coverUri,
-            modifier = Modifier
-                .weight(0.45F),
-        )
-
         val book = bookProgressWithChapters.book
 
-        Column(
-            modifier = Modifier
-                .weight(0.35F)
-                .padding(start = 12.dp, end = 12.dp, top = 8.dp),
+        Box(
+            modifier = Modifier.weight(0.6F),
         ) {
+            CoverImage(
+                coverUri = bookProgressWithChapters.book.coverUri,
+            )
+
             book.author?.let { author ->
                 Text(
                     text = author.uppercase(),
-                    modifier = Modifier.padding(top = 6.dp),
+                    modifier = Modifier.fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        .background(authorBackgroundColor),
+                    textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.labelSmall,
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis,
                 )
             }
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(0.4F)
+                .padding(start = 12.dp, end = 12.dp, top = 8.dp, bottom = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+        ) {
 
             Text(
                 text = book.title,
                 lineHeight = 1.2.em,
-                modifier = Modifier.fillMaxWidth().padding(top = 4.dp),
+                modifier = Modifier.fillMaxWidth(),
                 style = MaterialTheme.typography.titleSmall,
                 maxLines = 3,
                 overflow = TextOverflow.Ellipsis,
             )
-        }
 
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.End,
-            modifier = Modifier
-                .weight(0.2F)
-                .fillMaxWidth()
-                .padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
-        ) {
-            Text(
-                text = book.duration.format() ?: "",
-                style = MaterialTheme.typography.labelSmall,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-            )
-
-            if (bookProgressWithChapters.bookProgress.totalChapters > 0) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 2.dp),
+            ) {
                 Text(
-                    text =  bookProgressWithChapters.bookProgress.chapterProgressDisplayFormat(),
-                    modifier = Modifier.padding(top = 4.dp),
+                    modifier = Modifier.weight(0.4F),
+                    textAlign = TextAlign.Start,
+                    text = book.duration.format() ?: "",
                     style = MaterialTheme.typography.labelSmall,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
                 )
+
+                if (bookProgressWithChapters.bookProgress.totalChapters > 0) {
+                    Text(
+                        modifier = Modifier.weight(0.6F),
+                        textAlign = TextAlign.End,
+                        text =  bookProgressWithChapters.bookProgress.chapterProgressDisplayFormat(),
+                        style = MaterialTheme.typography.labelSmall,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun GridBookViewPreview() {
+    GridBookView(
+        bookProgressWithChapters = BookProgressWithBookAndChapters(
+            book = Book.empty().copy(
+                title = "Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title Book Title",
+                author = "Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author Author",
+                duration = ContentDuration(2.hours),
+            ),
+            chapter = Chapter.empty().copy(
+                title = "Chapter 1",
+                duration = ContentDuration(1.hours),
+            ),
+            chapters = listOf(
+                Chapter.empty().copy(
+                    title = "Chapter 1",
+                    duration = ContentDuration(1.hours),
+                ),
+                Chapter.empty().copy(
+                    title = "Chapter 2",
+                    duration = ContentDuration(1.hours),
+                ),
+            ),
+            bookProgress = BookProgress.empty().copy(
+                currentChapter = 1,
+                totalChapters = 2,
+            ),
+        ),
+    )
 }
