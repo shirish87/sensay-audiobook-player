beb78a3de0f7210de9f51ee0f0d21f9072c93b4d
Shirish Kamath <shirishk.87@gmail.com>
2022-09-13 21:50:38 +0530

    LinearProgressIndicator for busy state

--- app/src/main/kotlin/com/dotslashlabs/sensay/ui/screen/home/HomeAppBar.kt
+++ app/src/main/kotlin/com/dotslashlabs/sensay/ui/screen/home/HomeAppBar.kt
@@ -1,5 +1,6 @@
 package com.dotslashlabs.sensay.ui.screen.home
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Book
 import androidx.compose.material.icons.outlined.GridView
@@ -8,6 +9,8 @@ import androidx.compose.material.icons.outlined.ViewList
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.dotslashlabs.sensay.ui.screen.Destination
@@ -20,53 +23,70 @@ fun HomeAppBar(
     homeNavController: NavHostController,
     isBusy: Boolean,
     activeLayout: HomeLayout,
-    onScanCancel: () -> Unit,
+    @Suppress("UNUSED_PARAMETER") onScanCancel: () -> Unit,
     onSources: () -> Unit,
     onSettings: () -> Unit,
     onChangeLayout: (layout: HomeLayout) -> Unit,
 ) {
 
-    val navBackStackEntry by homeNavController.currentBackStackEntryAsState()
-    val isLibraryRoute = (navBackStackEntry?.destination?.route == Destination.Home.Library.route)
+    ConstraintLayout(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        val (appBar, loading) = createRefs()
 
-    SmallTopAppBar(
-        title = { Text("Sensay") },
-        actions = {
-            if (isBusy) {
-                IconButton(onClick = onScanCancel) {
-                    CircularProgressIndicator()
-                }
-            }
-            IconButton(onClick = {
-                onChangeLayout(
-                    when (activeLayout) {
-                        HomeLayout.LIST -> HomeLayout.GRID
-                        HomeLayout.GRID -> HomeLayout.LIST
-                    }
-                )
-            }) {
-                Icon(
-                    imageVector = when (activeLayout) {
-                        HomeLayout.LIST -> Icons.Outlined.GridView
-                        HomeLayout.GRID -> Icons.Outlined.ViewList
-                    },
-                    contentDescription = "",
-                )
-            }
-            if (isLibraryRoute) {
-                IconButton(onClick = onSources) {
+        val navBackStackEntry by homeNavController.currentBackStackEntryAsState()
+        val isLibraryRoute =
+            (navBackStackEntry?.destination?.route == Destination.Home.Library.route)
+
+        SmallTopAppBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(appBar) {
+                    top.linkTo(parent.top)
+                },
+            title = { Text("Sensay") },
+            actions = {
+                IconButton(onClick = {
+                    onChangeLayout(
+                        when (activeLayout) {
+                            HomeLayout.LIST -> HomeLayout.GRID
+                            HomeLayout.GRID -> HomeLayout.LIST
+                        }
+                    )
+                }) {
                     Icon(
-                        imageVector = Icons.Outlined.Book,
+                        imageVector = when (activeLayout) {
+                            HomeLayout.LIST -> Icons.Outlined.GridView
+                            HomeLayout.GRID -> Icons.Outlined.ViewList
+                        },
                         contentDescription = "",
                     )
                 }
+                if (isLibraryRoute) {
+                    IconButton(onClick = onSources) {
+                        Icon(
+                            imageVector = Icons.Outlined.Book,
+                            contentDescription = "",
+                        )
+                    }
+                    IconButton(onClick = onSettings) {
+                        Icon(
+                            imageVector = Icons.Outlined.Settings,
+                            contentDescription = "",
+                        )
+                    }
+                }
             }
-            IconButton(onClick = onSettings) {
-                Icon(
-                    imageVector = Icons.Outlined.Settings,
-                    contentDescription = "",
-                )
-            }
+        )
+
+        if (isBusy) {
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(loading) {
+                        top.linkTo(appBar.bottom)
+                    },
+            )
         }
-    )
+    }
 }
