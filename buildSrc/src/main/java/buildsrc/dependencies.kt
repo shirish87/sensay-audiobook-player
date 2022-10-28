c2616f899fbf38a655e7a70412625a24a4f6be0a
Shirish Kamath <shirishk.87@gmail.com>
2022-10-28 15:57:25 +0530

    Bump dependencies

--- buildSrc/src/main/java/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/buildsrc/dependencies.kt
@@ -66,11 +66,11 @@ object Libraries {
     }
 
     object Compose {
-        private const val version = "1.3.0-rc01"
-        private const val material3ComposeVersion = "1.0.0-rc01"
+        private const val version = "1.3.0"
+        private const val material3ComposeVersion = "1.0.0"
 
-        const val activityCompose = "androidx.activity:activity-compose:1.5.1"
-        const val navigationCompose = "androidx.navigation:navigation-compose:2.5.1"
+        const val activityCompose = "androidx.activity:activity-compose:1.6.1"
+        const val navigationCompose = "androidx.navigation:navigation-compose:2.5.3"
 
         const val foundation = "androidx.compose.foundation:foundation:$version"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.1"
